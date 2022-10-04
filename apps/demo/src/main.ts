5cef715de5714f98e1f9485ed69be675dbf38eb7
Bram Borggreve <beeman@beeman.nl>
2022-10-04 00:35:13 -0500

    fix: remove polyfills

--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -2,11 +2,6 @@ import { Keypair } from '@kin-kinetic/keypair';
 import { KineticSdk } from '@kin-kinetic/sdk';
 
 window.onload = () => {
-  const Buffer = require('buffer').Buffer;
-  (window as any).Buffer = Buffer;
-  (window as any).global = window;
-  (window as any).global.Buffer = Buffer;
-
   document.dispatchEvent(
     new CustomEvent('kinetic-sdk-ready', {
       detail: {

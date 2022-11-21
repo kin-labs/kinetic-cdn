70374f03a52a4dbb7728eb577020c6be1aa01d7c
Bram Borggreve <beeman@beeman.nl>
2022-11-21 17:13:14 +0000

    feat: update for Kinetic 1.0.0-rc.8

--- create-bundle.sh
+++ create-bundle.sh
@@ -1,5 +1,10 @@
 DIST="${PWD}/dist/apps/demo"
 
+ls -lh "$DIST"/runtime*.js
+ls -lh "$DIST"/polyfill*.js
+ls -lh "$DIST"/main*.js
+ls -lh "$DIST"/kinetic-sdk.js
+
 cat "$DIST"/runtime* "$DIST"/polyfill* "$DIST"/main* > "$DIST"/kinetic-sdk.js
 
 ls -lh "$DIST"/runtime*.js
